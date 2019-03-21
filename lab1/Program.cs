9c3adbf6fc0d0d62add8a6aa3befe6088d4ebd4d
Mixa Penchev <mixa.penchev@gmail.com>
2019-03-21 14:04:57 +0300

    fix

--- lab1/Program.cs
+++ lab1/Program.cs
@@ -10,6 +10,21 @@ namespace lab1
     {
         static void Main(string[] args)
         {
-        }
+            
+                int x, y;
+                double z;
+
+                //x- первое числа
+                //y - второе числа
+                // z - результат
+                Console.WriteLine("Введите ваше первое  число.");
+                x = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Ваше число : " + x);
+                Console.WriteLine("Введите ваше второе число.");
+                y = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Ваше число : " + y);
+                z = Math.Pow(x, 2) + Math.Pow(y, 2);
+                Console.WriteLine("a^2+b^2 = " + z);
+            }
     }
 }
